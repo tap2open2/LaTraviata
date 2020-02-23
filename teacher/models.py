706a4fb3a751922796d7a35fc1d4b96419310c64
mark <mark@getmorgana.com>
2020-02-23 12:58:02 +0200

    model teaching app

--- teacher/models.py
+++ teacher/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Stone(models.Model):
+    title = models.CharField(max_length=200)
+    summary = models.CharField(max_length=2000)
+    audio_summary = models.FileField(upload_to='audio/')
+
+
+class Stone_samples(models.Model):
+    stone = models.ForeignKey(Stone, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='stones')
--- teacher/views.py
+++ teacher/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.admin.views.decorators import staff_member_required
 
-# Create your views here.
+from django.http import HttpResponse
+
+@staff_member_required
+
+def index(request):
+    return HttpResponse("you are admin")
